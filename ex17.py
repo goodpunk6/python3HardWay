83cfae5bc5be2f06de6aac1395a99483ee4e9082
Mark Lotspaih <lotspaih@users.noreply.github.com>
2018-02-08 15:06:51 -0500

    Exercise 17

--- ex17.py
+++ ex17.py
@@ -9,15 +9,18 @@ print(f"Copying from {from_file} to {to_file}")
 in_file = open(from_file)
 indata = in_file.read()
 
-# One line option?
+# One line option from above?
 # in_file = open(from_file).read()
-# Change indata variable below to in_file
+# Change indata variable below to in_file and remove in_file close statment
 
+# To make this script more friendly, remove all lines below up to and including
+# the input() line:
 print(f"The input file is {len(indata)} bytes long")
 
 print(f"Does the output file exist? {exists(to_file)}")
 print("Ready, hit RETURN to continue, CTRL-C to abort.")
 input()
+# Stop removing here for friendly version
 
 out_file = open(to_file, 'w')
 out_file.write(indata)
@@ -26,3 +29,12 @@ print("Alright, all done.")
 
 out_file.close()
 in_file.close()
+
+# My super short version looks like this:
+# from sys import argv
+
+# script, from_file, to_file = argv
+
+# with open(from_file) as in_file:
+#     data = in_file.read()
+#     open(to_file, 'w').write(data)
